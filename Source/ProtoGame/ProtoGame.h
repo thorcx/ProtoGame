708224917fffe1a258b44988f34f11a3bc1f4fb3
thorcx <thorcx@163.com>
2017-03-26 15:34:35 +0800

    Init Game Project
    Create Game Project

---
+++ Source/ProtoGame/ProtoGame.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __PROTOGAME_H__
+#define __PROTOGAME_H__
+
+#include "EngineMinimal.h"
+
+#endif
---
+++ Source/ProtoGame/ProtoGameGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "ProtoGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProtoGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProtoGameGameMode();
+};
+
+
+
